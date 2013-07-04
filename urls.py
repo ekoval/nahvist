ea66d0ef0504f5eaf335887ef3b4beda9b3dbc64
ekoval <evgen.koval@gmail.com>
2013-07-04 23:10:29 +0300

    Added templates for Django-registration.

--- urls.py
+++ urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
+    (r'^accounts/', include('registration.urls')),
 )
